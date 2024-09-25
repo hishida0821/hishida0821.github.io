7be20b192bc5e8ebdcda66471c3d02928c5e1751
hishida0821 <hishida1204@gmail.com>
2024-09-25 15:43:02 +0900

    日付調整

--- app/works/page.js
+++ app/works/page.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Link from 'next/link';
 import client from "@/libs/client";
 
+const formatDate = (dateString) => {
+  if (!dateString) return '不明';
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString('ja-JP', options).replace(/\//g, '.');
+  return formattedDate;
+};
+
 async function fetchWorks() {
   const data = await client.get({ endpoint: 'works'})
   return data.contents;
@@ -16,7 +24,18 @@ const works = async () =>  {
       <ul>
         {worksData.map((work) => (
           <li key={work.id}>
-            <Link href={`/works/${work.id}`}>{work.title}</Link>
+            <Link href={`/works/${work.id}`}>
+              {work.eyecatch && (
+                <figure>
+                  <img
+                    src={work.eyecatch.url}
+                    alt={work.eyecatch.alt || 'アイキャッチ画像'}
+                  />
+                </figure>
+              )}
+              <span>{formatDate(work.publishedAt)}</span>
+              <p>{work.title}</p>
+              </Link>
           </li>
         ))}
       </ul>
